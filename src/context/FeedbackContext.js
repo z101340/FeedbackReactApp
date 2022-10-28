3bfeccf68946afb1a8b88356bf3301ee67f99578
unknown <z101340@outlook.com>
2022-10-28 14:57:09 -0400

    add JSON server and fetch data feature

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,17 +1,31 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
 // uuid package
 import { v4 as uuidv4 } from "uuid";
 
 const FeedbackContext = createContext()
 
 export const FeedbackProvider = ({children}) => {
-    const [feedback, setFeedback] = useState([
-        {
-            id:1,
-            text:"from context",
-            rating: 10
-        }
-    ])
+    const [feedback, setFeedback] = useState([])
+    const [feedbackEdit, setFeedbackEdit] = useState({
+        item: {},
+        edit: false
+    })
+    const [isLoading, setIsLoading] = useState(true)
+
+    // run when page load
+    useEffect (() => {
+        fetchFeedback()
+    }, [])
+
+    // fetch feedback
+    const fetchFeedback = async () => {
+        const response = await fetch("http://localhost:5000/feedback?_sort=id&_order=desc")
+        const data = await response.json
+        setFeedback(data)
+        setIsLoading(false)
+    }
+
+
 
     const addFeedback = (newFeedback) => {
         newFeedback.id = uuidv4()
@@ -24,11 +38,6 @@ export const FeedbackProvider = ({children}) => {
             setFeedback(feedback.filter((item) => item.id !== id))
         }
     }
-
-    const [feedbackEdit, setFeedbackEdit] = useState({
-        item: {},
-        edit: false
-    })
 
     const editFeedback = (item) => {
         setFeedback({
@@ -47,6 +56,7 @@ export const FeedbackProvider = ({children}) => {
     <FeedbackContext.Provider value={
         {feedback,
         feedbackEdit,
+        isLoading,
         deleteFeedback,
         addFeedback,
         editFeedback,
